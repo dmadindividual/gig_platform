f3d6474a6758aa446dd8c314b7d6cc11e96e07e2
afalade <afalade@paga.com>
2025-12-22 02:42:48 +0100

    Continued buulding user-service, added security configs

--- user-service/build.gradle.kts
+++ user-service/build.gradle.kts
@@ -1,28 +1,21 @@
 dependencies {
     implementation(project(":shared-kernel"))
-    
+
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    
+
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     runtimeOnly("org.postgresql:postgresql")
-    
+
+    implementation("org.flywaydb:flyway-core")
+
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("io.jsonwebtoken:jjwt-api:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-    
+
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    
+
     testImplementation("org.testcontainers:postgresql")
     testImplementation("org.testcontainers:testcontainers")
-}
-
-// ADD THIS - Temporary until we create main class
-tasks.named<org.springframework.boot.gradle.tasks.bundling.BootJar>("bootJar") {
-    enabled = false
-}
-
-tasks.named<Jar>("jar") {
-    enabled = true
 }
